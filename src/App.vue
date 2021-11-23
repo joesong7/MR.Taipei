3269fce4aed0fe75e6d94111e5248cb1ba3cdeb9
joesong <songrongyu@songrongyudeMacBook-Air.local>
2021-11-23 14:41:05 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,138 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app :class="$style.vApp">
+   
+    <div :class="$style.card">
+      <div class="card-body">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <div :class="$style.colt">
+                <v-title> 起點</v-title>
+   
+                <br />
+                <v-text>租屋處</v-text>
+              </div>
+            </div>
+            <div class="col">
+              <div :class="$style.colt">
+                <v-title> 目的地</v-title>
+
+                <br />
+                <v-text>實踐大學台北校區</v-text>
+              </div>
+            </div>
+            <div class="col"></div>
+          </div>
+        </div>
+        <div class="progress">
+          <div
+            role="progressbar"
+            :class="$style.probar1"
+            aria-valuenow="15"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          ></div>
+          <div
+            role="progressbar"
+            :class="$style.probar2"
+            aria-valuenow="30"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          ></div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <div :class="$style.colt">
+                <v-title> 出發時間</v-title>
+                <br />
+                <v-text>10:25</v-text>
+              </div>
+            </div>
+            <div class="col">
+              <div :class="$style.colt">
+                <v-title>預約時間</v-title>
+                <br />
+                <v-text>10:50</v-text>
+              </div>
+            </div>
+            <div class="col">
+              <div :class="$style.colt">
+                <v-text
+                  style="color: #17e68e; font-weight: bold; font-size: 30px"
+                  >25</v-text
+                >
+
+                <v-text>分鐘</v-text>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br />
+    <div :class="$style.btn">
+      <button type="button" class="btn btn-success">詳細資訊</button>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  props: {
+    msg: String,
+  },
+  setup() {
+    const data = reactive({
+      username: "joesong",
+    });
+    return { data };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+<style module>
+.card {
+  width: 100%;
+  opacity: 0.95;
+  border-radius: 20px;
+  background-color: #40434b;
+  margin-top: 539px;
+}
+.colt {
+  width: 150%;
+}
+.colt v-title {
+  font-size: 13px;
+  color: #aaaaaa;
+}
+.colt v-text {
+  color: #ffffff;
+}
+.probar1 {
+  width: 50%;
+
+  background-color: #f66b6f;
+}
+.probar2 {
+  width: 50%;
+
+  background-color: #e9a668;
+}
+.btn {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.btn button {
+  background-color: #40434b;
+  font-weight: bold;
+  color: #2e2d2d;
+  background: #17e68e;
+  border-radius: 20px;
+  width: 95%;
+  height: 49px;
+  text-align: center;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,43 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div class="card w-100">
+  <div class="card-body">
+    <div class="container">
+  <div class="row">
+    <div class="col">
+      起點
+      <br>
+      <v-text>租屋處</v-text>
+    </div>
+    <div class="col">
+      目的地
+      <br>
+       <v-text>實踐大學</v-text>
+    </div>
+    
   </div>
+</div>
+     <div class="progress" >
+
+  <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+  <div class="progress-bar bg-success" role="progressbar" style="width: 50%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+
+
+  
+
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+  </div>
+</div>
+
 </template>
 
 <script>
@@ -35,7 +45,15 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+ setup() {
+  
+  
+
+  
+
+
+}
 }
 </script>
 
